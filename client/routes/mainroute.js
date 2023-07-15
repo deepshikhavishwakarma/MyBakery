357e88688b315e97705e076c08a06cb2dfdf5b1f
Soumyadeep-Sinha <soumyadeep8402@gmail.com>
2023-07-15 22:52:34 +0530

    client side workflow partially working

--- client/routes/mainroute.js
+++ client/routes/mainroute.js
@@ -21,19 +21,55 @@ router.get("/seeMore/:cakeName", async (req, res) => {
     const cakeName = req.params.cakeName;
     // console.log(cakeName);
 
-    try{
-        const cake = await Cake.findOne({CakeName:cakeName});
+    try {
+        const cake = await Cake.findOne({ CakeName: cakeName });
         // console.log(cake);
-        res.render("description", {cake});
-    }catch(err){
+        res.render("description", { cake });
+    } catch (err) {
         console.error('Error fetching data:', err);
         // res.render("errorpage");
     }
 })
 
+router.get("/viewAll", async (req,res) => {
+    try {
+        const cakes = await Cake.find({}).exec();
+        console.log("Fetched cakes without error");
 
-router.get("/cart", async (req,res) => {
-    res.render("cart");
+
+        res.render("viewmore", { cakes });
+    } catch (err) {
+        console.error('Error fetching data:', err);
+        // res.render("errorpage"); 
+    }
 })
+
+
+router.post('/addtocart/:cakeName/:price', (req, res) => {
+    const { cakeName, price } = req.params;
+    const selectedSize = req.body.selectSize;
+    const thumbnailUrl = req.body.thumbnailUrl;
+
+    // console.log(selectedSize);
+
+    const newItem = {
+        cakeName,
+        price: parseInt(price) + parseInt(selectedSize),
+        size: selectedSize,
+        thumbnailUrl
+    };
+
+    if (!req.session.basket) {
+        req.session.basket = [newItem];
+    } else {
+        req.session.basket.push(newItem);
+    }
+    res.redirect('/cart');
+});
+
+router.get('/cart', (req, res) => {
+    const basketItems = req.session.basket || [];
+    res.render('cart', { basketItems });
+});
 
 module.exports = router;
